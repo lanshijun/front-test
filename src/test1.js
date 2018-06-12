29274b9be618e3b74b56089ffc81bac081585d4a
lanshijun <lanshijun@qzhsoft.com>
2018-06-12 14:15:05 +0800

    前端测试分析及代码构建

---
+++ src/test1.js
@@ -0,0 +1,10 @@
+﻿function firstLetterToUp(str){ 
+    return String(str).replace('^\s*|\s*$')
+                .replace(/\b\w+\b/g,function(word){ 
+                    word = word.toLowerCase();
+                    return word[0].toUpperCase() + word.substring(1);
+                });
+}
+
+var b = firstLetterToUp('  aBCDE Bda erew');
+console.log(b);
